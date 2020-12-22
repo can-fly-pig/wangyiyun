89ba380ddc5a5854966a973a9bcce9d85813958d
can-fly-pig <605092521@qq.com>
2020-12-22 16:09:38 +0800

    123

---
+++ backend/interface.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const conn = require('../dao/conn1.ts');
+
+const router = express.Router(); // 获得路由对象
+
+router.route('/getProducts')
+    .get((req, res, next) => {
+        let sql = 'select * from product';
+        conn.query(sql, (err, result) => {
+            if (err) {
+                err.status = 1001;
+                res.json(err)
+            };
+
+            let obj = {};
+            obj.status = 0;
+            obj.result = result;
+            res.json(obj);
+        });
+    });
+
+router.route("/login").get((req,res,next)=>{
+  res.json('helloWord1243523');
+  console.log(123);
+})
+
+module.exports = router; // 路由导出
